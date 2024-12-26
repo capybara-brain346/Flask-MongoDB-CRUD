d608c590880327ac105c88a2a3083dd6a26eb1b9
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-12-26 15:04:00 +0530

    added comments/docs

--- api/database.py
+++ api/database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 
+# return the collection that needs to be connected to
 def get_db():
     client = MongoClient("mongodb://mongodb:27017/")
     db = client["users"]
--- api/wsgi.py
+++ api/wsgi.py
@@ -1,3 +1,4 @@
+# wsgi server to replace the built-in flask server to make it production level
 from app import app
 
 if __name__ == "__main__":
